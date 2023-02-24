b76ac28c5a59cd0ac682ff6d70be047b5c17b1f9
PK <pradeepdhital124@gmail.com>
2023-02-24 12:43:30 +1100

    alert tab added on the form

--- api/src/routers/userRouter.js
+++ api/src/routers/userRouter.js
@@ -5,11 +5,17 @@ const router = express.Router();
 router.post("/", async (req, res, next) => {
   try {
     const result = await addUser(req.body);
-    res.json({
-      status: "success",
-      message: "Please check your email to verify your account",
-      result,
-    });
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "Your account has been successfully created, please go to login page and login with you email "+ result.email,
+          result, 
+        })
+      : res.json({
+          status: "error",
+          message: "unable to register the user, please try again later",
+          result,
+        });
   } catch (error) {
     if (error.message.includes("E11000 duplicate key error collection")) {
       error.status = 200;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { MainLayout } from "../components/layout/MainLayout";
 import { apiPostUser } from "../helpers/axiosHelper.js";
 import { toast } from "react-toastify";
+import { Alert } from "react-bootstrap";
 const initialState = {
   firstName: "",
   lastName: "",
@@ -14,6 +15,7 @@ const initialState = {
 };
 export const Register = () => {
   const [form, setForm] = useState(initialState);
+  const [resp, setResp] = useState({});
   const handleOnChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
@@ -28,7 +30,9 @@ export const Register = () => {
     if(confirmPassword != restForm.password){
       return alert("password not matching with confirm password")
     }
-    const {status, message} = await apiPostUser(restForm);
+    const { status, message } = await apiPostUser(restForm);
+    setResp({status, message}
+    )
     toast[status](message);
     status === "success" && setForm(initialState);
    
@@ -37,6 +41,12 @@ export const Register = () => {
     <MainLayout>
       <div className="form_page">
         <Form className="form_form border" onSubmit={handleOnSubmit}>
+          {resp.message && (
+            <Alert variant={resp.status === "success" ? "success" : "danger"}>
+              {resp.message}
+            </Alert>
+          )}
+
           <Form.Group className="mb-3" controlId="formBasicfirstName">
             <Form.Label>First Name</Form.Label>
             <Form.Control
